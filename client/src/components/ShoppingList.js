37eaca77f79cfd44b946f6b34717cd28dc6657d8
James Barrington <james.barrington1985@gmail.com>
2020-10-16 22:33:50 +0100

    Backend Routes Created & React Navbar Created

---
+++ client/src/components/ShoppingList.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import uuid from 'uuid';
+
+class ShoppingList extends Component {
+  state = {
+     items: [
+       { id: uuid(), name: 'Eggs' },
+       { id: uuid(), name: 'Milk' },
+       { id: uuid(), name: 'Steak' },
+       { id: uuid(), name: 'Water' }
+     ]
+  }
+
+  render() {
+    const { items } = this.state;
+    return (
+      <Container>
+        <Button
+          color="dark"
+          style={{marginBottom: '2rem'}}
+          onClick={() => {
+            const name = prompt('Enter Item');
+            if(name) {
+              this.setState(state => ({
+                items: [...state.items, { id: uuid(), name }]
+              }));
+            }
+          }}
+        >Add Item</Button>
+      </Container>
+    );
+  }
+}
+
+// Episode 4 8:04
+
+export default ShoppingList;
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb+srv://jbarrington:james123@shoppinglist.almtf.mongodb.net/<dbname>?retryWrites=true&w=majority'
+}
